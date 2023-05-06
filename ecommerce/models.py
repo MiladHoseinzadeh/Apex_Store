fc73b6d9e012ee1b2d3fa3bd65c88d0571ab84d8
Milad <milad.hz75@gmail.com>
2023-05-06 16:54:12 +0330

    fixed the Category model and added Product model

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -9,6 +9,25 @@ class Category(models.Model):
 
     class Meta:
         ordering = ("title",)
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.title
+    
+
+class Product(models.Model):
+	name = models.CharField(max_length=255)
+	slug = models.SlugField()
+	category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+	description = models.TextField(blank=True, null=True)
+	quantity = models.PositiveIntegerField()
+	price = models.IntegerField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		ordering = ("-created_at",)
+
+	def __str__(self):
+		return self.name
+
