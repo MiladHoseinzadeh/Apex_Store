6aee768af9438e2dd5f859c529eb521cfe453acb
Milad <milad.hz75@gmail.com>
2023-05-15 13:15:44 +0330

    added the settings and object of Cart

--- core/settings.py
+++ core/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Cart Settings
+SESSION_COOKIE_AGE = 86400
+
+CART_SESSION_ID = 'cart'
