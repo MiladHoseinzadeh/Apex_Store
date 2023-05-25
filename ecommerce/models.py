6450ceaa6fb7f91f12f32935cc4e7fd379d0c170
Milad <milad.hz75@gmail.com>
2023-05-26 00:42:22 +0330

    Fixed product images

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,4 +1,8 @@
+from io import BytesIO
+from django.core.files import File
 from django.db import models
+
+from PIL import Image
 
 
 class Category(models.Model):
@@ -22,6 +26,8 @@ class Product(models.Model):
 	description = models.TextField(blank=True, null=True)
 	quantity = models.PositiveIntegerField()
 	price = models.IntegerField()
+	image = models.ImageField(upload_to="uploads/", blank=True, null=True)
+	thumbnail = models.ImageField(upload_to="uploads/", blank=True, null=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
@@ -30,4 +36,28 @@ class Product(models.Model):
 
 	def __str__(self):
 		return self.name
+        
+	def get_thumbnail(self):
+		if self.thumbnail:
+			return self.thumbnail.url
+		else:
+			if self.image:
+				self.thumbnail = self.make_thumbnail(self.image)
+				self.save()
+
+				return self.thumbnail.url
+			else:
+				return "https://via.placeholder.com/300x200.jpg"
+	
+	def make_thumbnail(self, image, size=(300, 300)):
+		img = Image.open(image)
+		img.convert("RGB")
+		img.thumbnail(size)
+
+		thumb_io = BytesIO()
+		img.save(thumb_io, "JPEG", quality=85)
+
+		thumbnail = File(thumb_io, name=image.name)
+
+		return thumbnail
 
