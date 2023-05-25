8de82286988a45fa51f21d6ae825288d1e288dc2
Milad <milad.hz75@gmail.com>
2023-05-25 23:21:47 +0330

    Added login view and layout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accounts.views import signup
+from accounts.views import signup, login
 
 app_name = 'accounts'
 
 urlpatterns = [
 	path('signup/', signup, name='signup'),
+	path('login/', login, name='login'),
 ]
