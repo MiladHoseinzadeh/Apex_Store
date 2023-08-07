606e95a027b56eab181b5c6a8e665c848038892f
Milad <milad.hz75@gmail.com>
2023-08-07 23:45:42 +0330

    feat: added review database model

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -106,3 +106,11 @@ class OrderItem(models.Model):
 	updated_at = models.DateTimeField(auto_now=True)
 
 
+class Review(models.Model):
+	product = models.ForeignKey(Product, related_name="reviews", on_delete=models.CASCADE)
+	rating = models.IntegerField(default=3)
+	content = models.TextField()
+	created_by = models.ForeignKey(User, related_name="reviews", on_delete=models.CASCADE)
+	created_at = models.DateTimeField(auto_now_add=True)
+	
+
